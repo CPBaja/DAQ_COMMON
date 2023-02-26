b343c56805d85b00c08ba72ce12cb4f1a63b13c0
Dominic C <45153623+DominicChm@users.noreply.github.com>
2023-02-25 17:01:02 -0800

    add encoder,decoder

---
+++ decoder.h
@@ -0,0 +1,18 @@
+#pragma once
+
+template <typename T>
+
+class Decoder
+{
+    bool f_available;
+    T data;
+    T receiving;
+
+    Encoder()
+    {
+    }
+
+    const T *read()
+    {
+    }
+};
