e86f3e4cc0fe1b03316b77ac25e2c81109d90ad2
Lauren <laurendjones10@gmail.com>
2023-04-03 17:46:57 -0700

    encoder written for sleepy joe

--- encoder.h
+++ encoder.h
@@ -1,36 +1,36 @@
 #pragma once
 #include <Arduino.h>
 
-
 template <typename T_DATA, typename T_PREAMBLE = uint16_t, typename T_CHECKSUM = uint16_t, T_PREAMBLE V_PREAMBLE = 0x6969>
 class Encoder
 {
 
 private:
-    Stream *serial;
+    Stream &serial;
     bool _f_available;
 
-    size_t _num_to_send;
-    struct packet_t _packet;
+    size_t _bytes_sent;
 
-
+    union
+    {
+        packet_t data;
+        uint8_t buf[];
+    } _packet;
+    
     void packet_t_send(T_DATA dat)
     {
-        _packet.data = dat;
-        _packet.preamble = V_PREAMBLE;
-        _packet.checksum = calculate_cksum(&_packet.data);
+        _packet.data.data = dat;
+        _packet.data.preamble = V_PREAMBLE;
+        _packet.data.checksum = calculate_cksum(&dat);
+        _bytes_sent = 0;
     }
 
     // need to have space to write it to, and data to write -- check these two
     // then write a single byte while having those conditions 
     void fifo() 
     {
-        while(Serial.availableForWrite() > 0) {
-            bytes = Serial.availableForWrite();
-            value = Serial.readString();
-
-            Serial.println(value);
-            Serial.println(bytes);
+        if((_bytes_sent < sizeof(_packet)) && (serial.availableForWrite() > 0)) {
+            serial.write(_packet.buf[_bytes_sent++]);
         }
     }
 
@@ -40,14 +40,12 @@ private:
     // 3. Check if there is buffer space & place next byte of struct into serial
     void loop()
     {
-        void packet_t_send(T_DATA dat);
         fifo();
     }
 
 
 public:
-    Encoder(HardwareSerial *serial)
+    Encoder(Stream &s): serial(s)
     {
-        this->serial = serial;
     }
 };
