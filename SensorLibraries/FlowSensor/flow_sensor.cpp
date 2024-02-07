f2518e36b21c76c398a6104314b2efb1dc51645e
joekeenan13 <joekeenandev@gmail.com>
2024-02-07 15:09:05 -0800

    Resolving issues

--- SensorLibraries/FlowSensor/flow_sensor.cpp
+++ SensorLibraries/FlowSensor/flow_sensor.cpp
@@ -1,21 +1,21 @@
-#include "flow_sensor.h"
+// #include "flow_sensor.h"
 
-FlowSensor::FlowSensor(int8 pin) {
-    this->pin = pin;
-}
-
-void FlowSensor::begin() {
-    pinMode(pin, INPUT);
-}
-
-void FlowSensor::read() {
-    Sensor::cached = bitsToFlowRate(analogRead(pin));
-}
-
-// int16 FlowSensor::getCached() {
-//     return cached;
+// FlowSensor::FlowSensor(int8 pin) {
+//     this->pin = pin;
 // }
 
-int16 FlowSensor::bitsToFlowRate(uint16 bits) {
-    return flowLookup[bits];
-}
+// void FlowSensor::begin() {
+//     pinMode(pin, INPUT);
+// }
+
+// void FlowSensor::read() {
+//     Sensor::cached = bitsToFlowRate(analogRead(pin));
+// }
+
+// // int16 FlowSensor::getCached() {
+// //     return cached;
+// // }
+
+// int16 FlowSensor::bitsToFlowRate(uint16 bits) {
+//     return flowLookup[bits];
+// }
--- SensorLibraries/RotaryEncoder/RotaryEncoder.h
+++ SensorLibraries/RotaryEncoder/RotaryEncoder.h
@@ -1,20 +1,20 @@
-#ifndef __ROTARYPOT__
-#define __ROTARYPOT__
+// #ifndef __ROTARYPOT__
+// #define __ROTARYPOT__
 
-//#include <Arduino.h>
-#include "../Sensor/Sensor.h"
-#include "../../types.h"
+// //#include <Arduino.h>
+// #include "../Sensor/Sensor.h"
+// #include "../../types.h"
 
-class RotaryPot : public Sensor{
-public: 
-    RotaryPot();
+// class RotaryPot : public Sensor{
+// public: 
+//     RotaryPot();
 
-    void begin();
-    void read();
-    //int16 getCached();
+//     void begin();
+//     void read();
+//     //int16 getCached();
 
-private: 
-    int8 pin;
-    //int16 cachedPosition;
-}
-#endif
+// private: 
+//     int8 pin;
+//     //int16 cachedPosition;
+// }
+// #endif
