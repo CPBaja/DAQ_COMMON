6737d20f140279e5eb12008928ecd2742acbb273
edreinhardt <elladreinhardt@gmail.com>
2024-02-07 19:58:23 -0800

    fixed more stuff

--- SensorLibraries/FlowSensor/flow_sensor.cpp
+++ SensorLibraries/FlowSensor/flow_sensor.cpp
@@ -1,21 +1,17 @@
-// #include "flow_sensor.h"
+#include "flow_sensor.h"
 
-// FlowSensor::FlowSensor(int8 pin) {
-//     this->pin = pin;
-// }
+FlowSensor::FlowSensor(int8 pin) {
+    this->pin = pin;
+}
 
-// void FlowSensor::begin() {
-//     pinMode(pin, INPUT);
-// }
+void FlowSensor::begin() {
+    pinMode(pin, INPUT);
+}
 
-// void FlowSensor::read() {
-//     Sensor::cached = bitsToFlowRate(analogRead(pin));
-// }
+void FlowSensor::read() {
+    Sensor::cached = bitsToFlowRate(analogRead(pin));
+}
 
-// // int16 FlowSensor::getCached() {
-// //     return cached;
-// // }
-
-// int16 FlowSensor::bitsToFlowRate(uint16 bits) {
-//     return flowLookup[bits];
-// }
+int16 FlowSensor::bitsToFlowRate(uint16 bits) {
+    return flowLookup[bits];
+}
--- SensorLibraries/RotaryEncoder/RotaryEncoder.cpp
+++ SensorLibraries/RotaryEncoder/RotaryEncoder.cpp
@@ -1,18 +1,13 @@
+#include "RotaryEncoder.h"
 
-// #include "RotaryEncoder.h"
+RotaryPot::RotaryPot(int8 pin) {
+    this->pin = pin;
+}
 
-// RotaryPot::RotaryPot(int8 pin) {
-//     this->pin = pin;
-// }
+void RotaryPot::begin() {
+    pinMode(pin, INPUT);
+}
 
-// void RotaryPot::begin() {
-//     pinMode(pin, INPUT);
-// }
-
-// void RotaryPot::read() {
-//     Sensor::cached = (analogRead(pin)-102)*(105/819);
-// }
-
-// // int16 RotaryPot::getCached() {
-// //     return cachedPosition;
-// // }
+void RotaryPot::read() {
+    RotaryPot::cached = (analogRead(pin)-102)*(105/819);
+}
