5646c7049c6dad7347eb1539c47c40e2c1dbe50a
GavMeist3r <gstoddard5754@gmail.com>
2024-04-04 02:15:34 -0700

    goofy fix

--- can_address.h
+++ can_address.h
@@ -34,31 +34,31 @@ const uint16 IO = 0x3FF;
 
 const uint16 VOLTAGES = 0x400;
 
-const uint16 FL_SPEED = 0xBB8;
-const uint16 FR_SPEED = 0xBB9;
+const uint16 FL_SPEED = 0x64;
+const uint16 FR_SPEED = 0x65;
 
-const uint16 FR_SUS_POS = 0xBBA;
-const uint16 FL_SUS_POS = 0xBBB;
-const uint16 RR_SUS_POS = 0xBBC;
-const uint16 RL_SUS_POS = 0xBBD;
-const uint16 ST_POS = 0xBBE;
+const uint16 FR_SUS_POS = 0x66;
+const uint16 FL_SUS_POS = 0x67;
+const uint16 RR_SUS_POS = 0x68;
+const uint16 RL_SUS_POS = 0x69;
+const uint16 ST_POS = 0x6A;
 
-const uint16 CLUTCH_PRESS = 0xFA0;
-const uint16 PROP_SPEED = 0xFA1;
-const uint16 G_OIL_TEMP = 0xFA2;
-const uint16 SPOOL_TEMP = 0xFA3;
+const uint16 CLUTCH_PRESS = 0xC8;
+const uint16 PROP_SPEED = 0xC9;
+const uint16 G_OIL_TEMP = 0xCA;
+const uint16 SPOOL_TEMP = 0xCB;
 
-const uint8 SAS_HARD = 0x1770;
-const uint8 SAS_SOFT = 0x1771;
+const uint8 SAS_HARD = 0x30;
+const uint8 SAS_SOFT = 0x31;
 
-const uint16 IMU_ACCEL_X = 0x1B58;
-const uint16 IMU_ACCEL_Y = 0x1B59;
-const uint16 IMU_ACCEL_Z = 0x1B5A;
-const uint16 IMU_PITCH = 0x1B5B;
-const uint16 IMU_YAW = 0x1B5C;
-const uint16 IMU_ROLL = 0x1B5D;
-const uint16 IMU_LATITUDE = 0x1B5E;
-const uint16 IMU_LONGITUDE = 0x1B5F;
-const uint16 IMU_MAG_HEADING = 0x1B60;
+const uint16 IMU_ACCEL_X = 0x190;
+const uint16 IMU_ACCEL_Y = 0x191;
+const uint16 IMU_ACCEL_Z = 0x192;
+const uint16 IMU_PITCH = 0x193;
+const uint16 IMU_YAW = 0x194;
+const uint16 IMU_ROLL = 0x195;
+const uint16 IMU_LATITUDE = 0x196;
+const uint16 IMU_LONGITUDE = 0x197;
+const uint16 IMU_MAG_HEADING = 0x198;
 
 #endif
